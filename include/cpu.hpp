f1bef8a8d81806894b17e689e9ed27ad277caa10
Ryan Smith <rysmith2113@gmail.com>
2025-03-05 11:34:54 -0500

    Add the scheduler

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -8,8 +8,9 @@
 #include <thread>
 #include <vector>
 
-#include "process.hpp"
 #include "circular_linked_list.hpp"
+#include "process.hpp"
+#include "types.hpp"
 
 using std::vector;
 
@@ -20,16 +21,18 @@ class CPU {
 			currentProcess(readyQueue.getHead())
 		{}
 
+		void roundRobin(int quantum);
+	private:
 		void tick() {
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			_tick++;
 		}
-	
-	private:
+
 		unsigned int _tick = 0;
-		
+		constexpr static int OVERHEAD_TIME = 2;
+
 		CircularLinkedList<Process*> readyQueue;
 		CircularLinkedList<Process*> finishedQueue = {};
-		
+
 		typename decltype(readyQueue)::node_type currentProcess;
 };
---
+++ src/cpu.cpp
@@ -0,0 +1,40 @@
+//
+// Filename: cpu.cpp
+// Description: The CPU
+// Created: 3/4/25
+//
+
+#include "cpu.hpp"
+
+#include "io.hpp"
+
+void CPU::roundRobin(int quantum) {
+	for (
+		auto process = currentProcess;
+		process != nullptr && process->next != nullptr;
+		process = process->next
+	) {
+		io::recordEvent(_tick, std::format("Process {} running", process->data->processID));
+
+		for (int ticks = quantum; process->data->burstTime > 0 && ticks > 0; ticks--) {
+			tick();
+			process->data->burstTime--;
+		}
+
+		if (process->data->burstTime == 0) {
+			io::recordEvent(_tick, std::format("Process {} finished", process->data->processID));
+
+			// Move the process to the finished queue
+			finishedQueue.insertAtEnd(process->data);
+			readyQueue.deleteByValue(process->data);
+		}
+		else {
+			io::recordEvent(_tick, std::format("Process {} blocking", process->data->processID));
+		}
+
+		io::recordEvent(_tick, "Switching context");
+		_tick += OVERHEAD_TIME; // Simulate process-switching overhead
+	}
+
+	io::recordEvent(_tick, "All processes finished. Shutting down.");
+}
