afb15d834ba84da43e513097aa8cfd85ea1a2dc1
Ryan Smith <rysmith2113@gmail.com>
2025-03-06 16:11:43 -0500

    Output the number of ticks and the total real-world time taken to run the simulation

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,15 @@
 // Created: 3/3/25
 //
 
+#include <chrono>
+#include <format>
 #include <iostream>
 
 #include "cpu.hpp"
 #include "io.hpp"
 #include "process.hpp"
+
+namespace chrono = std::chrono;
 
 int main() {
 	io::createLog();
@@ -25,13 +29,25 @@ int main() {
 				  << " | Burst time: " << process->burstTime
 				  << "\n";
 	}
+	std::cout << "\nRunning simulation..." << std::flush;
 
-	std::cout << "\nRunning simulation...\n";
+	constexpr static unsigned int QUANTUM = 30;
+	constexpr static unsigned int OVERHEAD = 2;
 
-	CPU cpu(processes); // Create the CPU
-	cpu.roundRobin(5);	// Run the simulation
+	CPU cpu(QUANTUM, OVERHEAD, processes); // Create the CPU
 
-	std::cout << "Simulation complete. Audit log saved to " << io::LOG_PATH << ".\n";
+	// Run  and time the simulation
+	auto start = chrono::high_resolution_clock::now();
+	cpu.roundRobin();
+	auto end = chrono::high_resolution_clock::now();
+
+	auto duration = end - start;
+	auto minutes = duration_cast<chrono::minutes>(duration).count();
+    auto seconds = duration_cast<chrono::seconds>(duration % chrono::minutes(1)).count();
+
+	std::cout << "\n\nSimulation complete in " << cpu.getTickCount() << " ticks "
+			  << std::format("({}m {}s).", minutes, seconds)
+			  << "\nAudit log saved to " << io::LOG_PATH << ".\n";
 
 	return 0;
 }
